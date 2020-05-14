fbeed44b23446f0fed97b243ea31359601a3e274
titeek <krystian.bendinger@gmail.com>
2020-05-14 23:49:33 +0200

    added option for all orientations and sizes

--- app/src/main/java/com/example/astro/MainActivity.java
+++ app/src/main/java/com/example/astro/MainActivity.java
@@ -14,12 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button optionsButton = (Button) findViewById(R.id.optionsButton);
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent optionsIntent = new Intent(getApplicationContext(), Options.class);
+                startActivity(optionsIntent);
+            }
+        });
+
         Button aboutButton = (Button) findViewById(R.id.aboutButton);
         aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                     Intent aboutIntent = new Intent(getApplicationContext(), About.class);
                     startActivity(aboutIntent);
+            }
+        });
+
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    System.exit(0);
             }
         });
     }
